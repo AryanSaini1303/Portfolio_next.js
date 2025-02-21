89cecf65601cb3107bf191946c09160dd27c2fed
AryanSaini1303 <saini.aryan9999@gmail.com>
2025-02-21 22:08:29 +0530

    Style:Added new images and update HeroSection layout with road map and tech stack section

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -9,11 +9,16 @@ const playwrite = Playwrite_US_Modern({
 export default function HeroSection() {
   return (
     <section className="mainGridContainer my-10">
-      <div className="bg-[#1a1a1a] rounded-2xl shadow-md shadow-black grid1 flex justify-between text-white py-7 px-10 gap-16">
+      <div className="bg-[#1a1a1a] rounded-2xl shadow-md shadow-black grid1 flex justify-between text-white py-7 px-10 gap-16 items-center">
         <div className="flex flex-col items-end">
           <div>
-            <h1 className="font-concert text-7xl z-10 before:content-[''] px-3 before:bg-[#FF4C29] before:absolute relative before:h-[40%] before:w-full before:bottom-0 before:blur-custom before:rounded-full before:z-[-1] before:left-0 w-fit">Fit to code,</h1>
-            <h1 className="font-concert text-7xl z-10 before:content-[''] px-3 before:bg-[#FF4C29] before:absolute relative before:h-[40%] before:w-full before:bottom-0 before:blur-custom before:rounded-full before:z-[-1] before:left-0 w-fit"> Built to Lead.</h1>
+            <h1 className="font-concert text-7xl z-10 before:content-[''] px-3 before:bg-[#FF4C29] before:absolute relative before:h-[40%] before:w-full before:bottom-0 before:blur-custom before:rounded-full before:z-[-1] before:left-0 w-fit">
+              Fit to code,
+            </h1>
+            <h1 className="font-concert text-7xl z-10 before:content-[''] px-3 before:bg-[#FF4C29] before:absolute relative before:h-[40%] before:w-full before:bottom-0 before:blur-custom before:rounded-full before:z-[-1] before:left-0 w-fit">
+              {" "}
+              Built to Lead.
+            </h1>
           </div>
           <p className="font-playwrite text-xs w-fit my-3">
             Blending technology with a problem-solving mindset,
@@ -22,14 +27,103 @@ export default function HeroSection() {
             <br /> Always striving to build and improve.
           </p>
         </div>
-        <div className="relative group flex-1 top-[-0.5rem]">
-          <Image src="/profile3.jpg" alt="profile 1" height={1000} width={1000} className="rounded-full border-[#F8D210] border-solid border-[6px] h-44 w-44 absolute z-10"/>
-          <Image src="/profile4.jpg" alt="profile 2" height={1000} width={1000} className="rounded-full border-[#00A8E8] border-solid border-[6px] h-44 w-44 absolute top-20 left-28"/>
-          <Image src="/heart.png" alt="heart" height={65} width={65} className="absolute top-[9rem] left-20 z-10 bg-[#121212] rounded-full p-2 border-[#FF4C29] border-[3px]"/>
+        <div className="relative group flex-1 top-[-8.5rem] left-5">
+          <Image
+            src="/profile3.jpg"
+            alt="profile 1"
+            height={1000}
+            width={1000}
+            className="rounded-full border-[#F8D210] border-solid border-[6px] h-48 w-48 absolute z-10"
+          />
+          <Image
+            src="/profile4.jpg"
+            alt="profile 2"
+            height={1000}
+            width={1000}
+            className="rounded-full border-[#00A8E8] border-solid border-[6px] h-48 w-48 absolute top-20 left-28"
+          />
+          <Image
+            src="/heart.png"
+            alt="heart"
+            height={75}
+            width={75}
+            className="absolute top-[9.8rem] left-20 z-10 bg-[#121212] rounded-full p-2 border-[#FF4C29] border-[3px]"
+          />
         </div>
       </div>
-      <div className="bg-[#1a1a1a] rounded-2xl shadow-md shadow-black"></div>
-      <div className="bg-[#1a1a1a] rounded-2xl shadow-md shadow-black"></div>
+      <div className="bg-[#1a1a1a] rounded-2xl shadow-md shadow-black">
+        <div className="flex flex-wrap gap-5 py-4 px-6 justify-center">
+          <Image
+            src={"/nextjs.png"}
+            alt="tech-stack"
+            width={100}
+            height={100}
+            className="h-[5.6rem] w-[5.6rem]"
+          />
+          <Image
+            src={"/python.png"}
+            alt="tech-stack"
+            width={100}
+            height={100}
+            className="h-[5.6rem] w-[5.6rem] mt-12"
+          />
+          <Image
+            src={"/react-native.png"}
+            alt="tech-stack"
+            width={100}
+            height={100}
+            className="h-[5.6rem] w-[5.6rem]"
+          />
+          <Image
+            src={"/vs-code.png"}
+            alt="tech-stack"
+            width={100}
+            height={100}
+            className="h-[5.6rem] w-[5.6rem]"
+          />
+          <Image
+            src={"/github.png"}
+            alt="tech-stack"
+            width={100}
+            height={100}
+            className="h-[5.6rem] w-[5.6rem] mt-12"
+          />
+          <Image
+            src={"/c++.png"}
+            alt="tech-stack"
+            width={100}
+            height={100}
+            className="h-[5.6rem] w-[5.6rem]"
+          />
+        </div>
+      </div>
+      <div className="bg-[#1a1a1a] rounded-2xl shadow-md shadow-black justify-center items-center flex overflow-hidden relative">
+        <Image
+          src={"/road.png"}
+          alt="road-map"
+          height={80}
+          width={600}
+          className="-rotate-12"
+        />
+        <div className="absolute flex-col top-12 left-2">
+          <h4 className="text-white font-playwrite">Stared journey as a web developer</h4>
+          <div className="relative left-[50%]">
+            <div className="h-16 w-[0.1rem] bg-[#808080] before:content-[''] before:absolute before:h-2 before:w-2 before:rounded-full before:bg-[#808080] before:bottom-0 before:left-[-0.18rem]"></div>
+          </div>
+        </div>
+        <div className="absolute flex-col justify-center -bottom-8">
+          <h4 className="text-white font-playwrite">Worked on freelance projects</h4>
+          <div className="relative left-[50%] top-[-5rem]">
+            <div className="h-14 w-[0.1rem] bg-[#808080] before:content-[''] before:absolute before:h-2 before:w-2 before:rounded-full before:bg-[#808080] before:top-0 before:left-[-0.18rem]"></div>
+          </div>
+        </div>
+        <div className="absolute flex-col justify-center right-6 top-4">
+          <h4 className="text-white font-playwrite">Became CTO at RYM Grenergy</h4>
+          <div className="relative left-[50%]">
+            <div className="h-16 w-[0.1rem] bg-[#808080] before:content-[''] before:absolute before:h-2 before:w-2 before:rounded-full before:bg-[#808080] before:bottom-0 before:left-[-0.18rem]"></div>
+          </div>
+        </div>
+      </div>
       <div className="bg-[#1a1a1a] rounded-2xl shadow-md shadow-black grid4"></div>
     </section>
   );
--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Navbar() {
   return (
     <section className="bg-foreground flex justify-between w-1/2 text-white mx-auto items-center p-2 rounded-full font-concert shadow-md shadow-black">
-      <div className="h-10 w-10 bg-green-500 rounded-full shadow-inner"><Image src="/panda.png" height={100} width={100} alt="panda"/>
+      <div className="h-10 w-10 bg-white rounded-full shadow-inner"><Image src="/panda.png" height={100} width={100} alt="panda"/>
       </div>
       <ul className="flex justify-between w-1/2 gap-6">
         <li className="relative group active:opacity-50 transition-all duration-150">
